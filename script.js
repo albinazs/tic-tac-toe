057f116bc6ed60a6a875c448256e61f5934f0b11
Albina Starykova <albina.starikova@gmail.com>
2022-08-05 13:18:31 +0200

    Add basic UI

---
+++ script.js
@@ -0,0 +1,16 @@
+const Player = () => {
+    // makeMove method
+    return {};
+}
+
+const gameBoard = (() => {
+    //array 3x3
+    const grid = {};
+    // render content - data-attr? 
+    return {};
+})()
+
+const displayController = (() => {
+    
+    return {};
+})()
