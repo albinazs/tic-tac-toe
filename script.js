266243b34104a8a7f1c98369fd569a92483fb319
Albina Starykova <albina.starikova@gmail.com>
2022-08-09 15:08:10 +0200

    Add function to clearGrid by clicking restart button

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 const gameBoard = (() => {
-    const grid = ['x', '', '', '', '', 'o', 'x', 'o', 'x'];
+    let grid = ['', '', '', '', '', '', '', '', ''];
+    const gotWinner = () => (gameBoard.grid[0] === gameBoard.grid[1] === gameBoard.grid[2]) &&
+    gameBoard.grid[0] !== '';
+    const gameOver = () => gameBoard.grid[0] === gameBoard.grid[1] === gameBoard.grid[2];
+    const clearGrid = () => {
+        gameBoard.grid = ['', '', '', '', '', '', '', '', ''];
+        //remove from here
+        displayController.render(gameBoard.grid);
+    };
     //getgrid instead of grid?????
-    return {grid};
+    return {grid, gotWinner, gameOver, clearGrid};
 })()
 
 const displayController = (() => {
     const gridItems = document.querySelectorAll('.grid-item');
     const render = (grid) => gridItems.forEach(item => item.textContent = grid[item.dataset.index]);
-    //remove gridItems?
     return {render, gridItems};
 })()
 
 const Player = (mark) => {
+    const test = document.querySelector('.test');
     const makeMove = (e) => {
-        if(gameBoard.grid[e.target.dataset.index] === '') {
+        if(typeof gameBoard.grid[e.target.dataset.index] !== 'null') {
             gameBoard.grid.splice(e.target.dataset.index, 1, mark);
             //remove from here
             displayController.render(gameBoard.grid);
-        }    
+        }  
+        if(gameBoard.gotWinner) {
+            test.style.backgroundColor = 'red';
+        } 
     };
     return {makeMove};
 }
@@ -25,11 +36,15 @@ const Player = (mark) => {
 const player1 = Player('x');
 const player2 = Player('o');
 
-displayController.gridItems.forEach(item => item.addEventListener('click', player2.makeMove));
+displayController.gridItems.forEach(item => item.addEventListener('click', player1.makeMove));
 
 displayController.render(gameBoard.grid);
 
-// grid-items add event listener for click => 0.player1 move - check if available. after player2 move
+const restart = document.querySelector('button');
+restart.addEventListener('click', gameBoard.clearGrid);
+
+
+// grid-items add event listener for click => displ.game => if array empty - player1.move, else if last mark was x, then o
 // DONE 1. gameboard array splice? based on data-attr, return array
 // DONE 2. displaycontroller.render returned array
 // 3. displaycontroller.message update 1.which player moves, 2.who is the winner 
@@ -41,4 +56,4 @@ displayController.render(gameBoard.grid);
 // or all moves were made (array is filled)
 // if array[] === &&array[] === &&array[], || ... => check contents and announce winner
 
-//restart - clear grid, render
+// DONE restart - clear grid, render
