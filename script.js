060b51166c777cf5a9c4f4cc2cb44092d80e40ca
Albina Starykova <albina.starikova@gmail.com>
2022-08-11 16:16:18 +0200

    Refactor makeMove in order to fix rounds counter

--- script.js
+++ script.js
@@ -7,40 +7,48 @@ const gameBoard = (() => {
 const displayController = (() => {
     const gridItems = document.querySelectorAll('.grid-item');
     const restart = document.querySelector('button');
+    const message = document.querySelector('.message>p');
 
     const render = (grid) => gridItems.forEach(item => item.textContent = grid[item.dataset.index]);
+
+    const updateMessage = () => message.textContent = `Player ${gameController.getCurrentPlayer().getMark()}'s turn`;
     
     restart.addEventListener('click', () => {
         gameBoard.clearGrid();
         displayController.render(gameBoard.grid);
     });
-    return {render, gridItems};
+    
+    return {render, gridItems, updateMessage};
 })()
 
 const Player = (mark) => {
-    const makeMove = (e) => {
-        if(typeof gameBoard.grid[e.target.dataset.index] !== 'null') {
-            gameBoard.grid.splice(e.target.dataset.index, 1, mark);
-            //remove from here
-            displayController.render(gameBoard.grid);
-        }  
-    };
     const getMark = () => mark;
-    return {makeMove, getMark};
+    return {getMark};
 }
 
 const gameController = (() => {
-    const player1 = Player('x');
-    const player2 = Player('o');
+    const player1 = Player('X');
+    const player2 = Player('O');
     let round = 1;
 
     const getCurrentPlayer = () => {
         return round % 2 === 1? player1 : player2;
     };
 
+    const gameOver = () => {
+        console.log(round);
+        return round === 9;
+    };
+
     const playRound = (e) => {
-        getCurrentPlayer().makeMove(e);
-        round++;
+        //getCurrentPlayer().makeMove(e);
+        if(gameBoard.grid[e.target.dataset.index] === '') {
+            gameBoard.grid.splice(e.target.dataset.index, 1, getCurrentPlayer().getMark()); 
+            round++;
+            console.log(round);
+        };
+        displayController.render(gameBoard.grid);
+        displayController.updateMessage();           
     };
 
     displayController.gridItems.forEach(item => item.addEventListener('click', playRound));
@@ -48,19 +56,19 @@ const gameController = (() => {
     //const gotWinner = () => (gameBoard.grid[0] === gameBoard.grid[1] === gameBoard.grid[2]) &&
     //gameBoard.grid[0] !== '';
     //const gameOver = () => gameBoard.grid[0] === gameBoard.grid[1] === gameBoard.grid[2];
-    return {playRound};
+    return {getCurrentPlayer};
 })()
 
-// grid-items add event listener for click => displ.game => if array empty - player1.move, else if last mark was x, then o
-// DONE 1. gameboard array splice? based on data-attr, return array
-// DONE 2. displaycontroller.render returned array
-// 3. displaycontroller.message update 1.which player moves, 2.who is the winner 
+console.log(gameController.round);
+gameController.round++;
+gameController.round++;
+gameController.round++;
+
+// 3. displaycontroller.message update who is the winner 
 
 // gameboard winner & gameover check: 8 cases of winning:
 // 0, 3, 6 / 1, 4, 7 / 2, 5, 8
 // 0, 1, 2 / 3, 4, 5 / 6, 7, 8
 // 0, 4, 8 / 2, 4, 6 
-// or all moves were made (array is filled)
+// or all moves were made (array is filled / counter is 10!)
 // if array[] === &&array[] === &&array[], || ... => check contents and announce winner
-
-// DONE restart - clear grid, render
